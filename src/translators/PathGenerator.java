f883063c1500e1f2271753c72abcfa9b70491abe
Ke <yke@yalinkemac.student.iastate.edu>
2014-10-22 22:35:01 -0500

    ,,

--- src/translators/PathGenerator.java
+++ src/translators/PathGenerator.java
@@ -20,13 +20,13 @@ public class PathGenerator {
 	private String fileName;
 	
 	private Map<String, String> source;
-	private Map<String, List<String>> paths;
+	private List<String> paths;
 	private Map<String, List<String>> inputs;
 	
 	public PathGenerator(String fileName){
 		this.fileName = fileName;
 		this.source = new HashMap<String, String>();
-		this.paths = new HashMap<String, List<String>>();
+		this.paths = new ArrayList<String>();
 	}
 	
 	
@@ -48,14 +48,26 @@ public class PathGenerator {
 		                 OutputStreamWriter(new FileOutputStream("temp")));
 			String s;
 			String method = null;
-			String path;
-			List<String> input;
+			String path = "";
+			List<String> input = new ArrayList<String>();
 			while((s = ls_in.readLine()) != null){
 				s = s.trim();
 				if(s.startsWith("processing:")){
 					if(method != null){
 						inputs.put(path, input);
-						//paths
+						paths.add(path);
+					}
+					else if(s.startsWith("LOCAL")){
+						path = path + "\n" + s.substring(6, s.length() - 1);
+					}
+					else if(s.startsWith("GLOBAL")){
+						path = path + "\n" + s.substring(7, s.length() - 1);
+					}
+					else if(s.startsWith("FORMAL")){
+						input.add(s.substring(7, s.length() - 1));
+					}
+					else if(s.startsWith("STMT(return")){
+						path = path + "\n" + s.substring(5, s.length() - 1);
 					}
 					method = s.substring(12);
 					//path = new ArrayList<String>();
