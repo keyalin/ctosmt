931b01e210512a34cd5064ceaf27f6aff0787d12
Ke <yke@yalinkemac.student.iastate.edu>
2014-10-22 22:08:53 -0500

    safety

--- src/translators/PathGenerator.java
+++ src/translators/PathGenerator.java
@@ -6,21 +6,27 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+/*
+ * need to redesign with method generator
+ */
 public class PathGenerator {
 	
 	private String fileName;
 	
 	private Map<String, String> source;
-	private Map<String, List<String>> path;
+	private Map<String, List<String>> paths;
+	private Map<String, List<String>> inputs;
 	
 	public PathGenerator(String fileName){
 		this.fileName = fileName;
 		this.source = new HashMap<String, String>();
-		this.path = new HashMap<String, List<String>>();
+		this.paths = new HashMap<String, List<String>>();
 	}
 	
 	
@@ -41,9 +47,20 @@ public class PathGenerator {
 		       BufferedWriter bw = new BufferedWriter(new
 		                 OutputStreamWriter(new FileOutputStream("temp")));
 			String s;
+			String method = null;
+			String path;
+			List<String> input;
 			while((s = ls_in.readLine()) != null){
-				bw.write(s);
-				bw.write("\n");
+				s = s.trim();
+				if(s.startsWith("processing:")){
+					if(method != null){
+						inputs.put(path, input);
+						//paths
+					}
+					method = s.substring(12);
+					//path = new ArrayList<String>();
+					input = new ArrayList<String>();
+				}
 			}
 			bw.flush();
 			ls_in.close();
